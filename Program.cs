8976c8646888d67ef048d15f8f2c29503b831ce2
rumeysaexe <rumeysa.main@gmail.com>
2024-11-16 22:40:14 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace kare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int kareBoyutu = 7; 
+            for (int i = 0; i < kareBoyutu; i++)
+            {
+                for (int j = 0; j < kareBoyutu; j++)
+                {
+                    if (i == 0 || i == kareBoyutu - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (j == 0 || j == kareBoyutu - 1)
+                        {
+                            Console.Write("*");
+                        }
+                        else
+                        {
+                            Console.Write(" ");
+                        }
+                    }
+                }
+                Console.WriteLine(); 
+            }
+
+        }
+    }
+}
